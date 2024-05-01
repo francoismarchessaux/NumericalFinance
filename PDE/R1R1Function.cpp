1a0925615101c311c4977f9e2fd1109fa90054fd
François Marchessaux <francois.marchessaux@edu.devinci.fr>
2024-05-01 12:19:03 +0200

    Add files via upload

--- PDE/R1R1Function.cpp
+++ PDE/R1R1Function.cpp
@@ -1,5 +1,51 @@
 #include "R1R1Function.h"
+#include <cmath>
+#include <algorithm>
 
-R1R1Function::R1R1Function(){}
+R1R1Function::R1R1Function() {}
 
 R1R1Function::~R1R1Function(){}
+
+/* VANILLA OPTIONS */
+VanillaTerminalCondition::VanillaTerminalCondition(double strike) : R1R1Function(), Strike(strike) {}
+
+// Terminal Conditions
+CallTerminalCondition::CallTerminalCondition(double strike) : VanillaTerminalCondition(strike) {}
+PutTerminalCondition::PutTerminalCondition(double strike) : VanillaTerminalCondition(strike) {}
+
+double CallTerminalCondition::operator()(double x)
+{
+	return std::max(x - Strike, 0.);
+}
+
+double PutTerminalCondition::operator()(double x)
+{
+	return std::max(Strike - x, 0.);
+}
+
+// Top and Bottom conditions
+
+CallTopBoundary::CallTopBoundary(double sMax, double strike) : R1R1Function(), SMax(sMax), Strike(strike) {}
+CallBottomBoundary::CallBottomBoundary(double sMin, double strike) : R1R1Function(), SMin(sMin), Strike(strike) {}
+PutTopBoundary::PutTopBoundary(double sMax, double strike) : R1R1Function(), SMax(sMax), Strike(strike) {}
+PutBottomBoundary::PutBottomBoundary(double sMin, double strike) : R1R1Function(), SMin(sMin), Strike(strike) {}
+
+double CallTopBoundary::operator()(double t)
+{
+	return std::max(SMax - Strike, 0.);
+}
+
+double CallBottomBoundary::operator()(double t)
+{
+	return std::max(SMin - Strike, 0.);
+}
+
+double PutTopBoundary::operator()(double t)
+{
+	return std::max(Strike - SMax, 0.);
+}
+
+double PutBottomBoundary::operator()(double t)
+{
+	return std::max(Strike - SMin, 0.);
+}
--- PDE/R1R1Function.h
+++ PDE/R1R1Function.h
@@ -3,7 +3,85 @@
 class R1R1Function
 {
 	public:
+		// Constructor
 		R1R1Function();
+
+		// Destructor
 		~R1R1Function();
+
+		// Methods
 		virtual double operator()(double x) = 0;
+};
+
+/*****  VANILLA OPTIONS *****/
+
+class VanillaTerminalCondition : public R1R1Function
+{
+	protected:
+		double Strike;
+
+	public:
+		VanillaTerminalCondition(double strike);
+};
+
+// CALL Option
+
+class CallTerminalCondition : public VanillaTerminalCondition
+{
+	public:
+		CallTerminalCondition(double strike);
+		double operator()(double x);
+};
+
+class CallTopBoundary : public R1R1Function
+{
+	private:
+		double SMax;
+		double Strike;
+
+	public:
+		CallTopBoundary(double sMax, double strike);
+		double operator()(double t);
+};
+
+class CallBottomBoundary : public R1R1Function
+{
+	private:
+		double SMin;
+		double Strike;
+
+	public:
+		CallBottomBoundary(double sMin, double strike);
+		double operator()(double t);
+};
+
+// PUT Option
+
+class PutTerminalCondition : public VanillaTerminalCondition
+{
+	public:
+		PutTerminalCondition(double strike);
+		double operator()(double x);
+};
+
+class PutTopBoundary : public R1R1Function
+{
+private:
+		double SMax;
+		double Strike;
+
+	public:
+		PutTopBoundary(double sMax, double strike);
+		double operator()(double t);
+};
+
+class PutBottomBoundary : public R1R1Function
+{
+	private:
+		double SMin;
+		double Strike;
+
+	public:
+		PutBottomBoundary(double sMin, double strike);
+		double operator()(double t);
 };
